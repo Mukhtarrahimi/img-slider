ec3a78494b6bd90350f903d5e290fdcb188af9bd
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-06 19:02:34 +0430

    create component of project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Services from './pages/services/Services'
+import Blog from './pages/blog/Blog'
+import Contact from './pages/contact/Contact'
+
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
