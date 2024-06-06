8fd19f11ed5bff82464ef39310623c26341a3b17
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-06 21:29:04 +0430

    carousel code update

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,44 +1,65 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { MdOutlineNavigateNext } from "react-icons/md";
 import { GrFormPrevious } from "react-icons/gr";
 
-
-function Carousel({children: images, autoImages= false, autoImagesInterval = 6000 }) {
-
-  const [curentImg , setCurentImg] = useState(0)
-  const prev = () =>{
-    setCurentImg((curentImg)=> (curentImg ===0 ? images.length-1: curentImg -1))
-  }
-  const next = () =>{
-    setCurentImg((curentImg)=> (curentImg === images.length-1 ? 0 : curentImg + 1))
-  }
+function Carousel({
+  children: images,
+  autoImages = false,
+  autoImagesInterval = 6000,
+}) {
+  const [curentImg, setCurentImg] = useState(0);
+  const prev = () => {
+    setCurentImg((curentImg) =>
+      curentImg === 0 ? images.length - 1 : curentImg - 1
+    );
+  };
+  const next = () => {
+    setCurentImg((curentImg) =>
+      curentImg === images.length - 1 ? 0 : curentImg + 1
+    );
+  };
   useEffect(() => {
-    if(!autoImages) return
-    const imagesInterval = setInterval(next , autoImagesInterval)
+    if (!autoImages) return;
+    const imagesInterval = setInterval(next, autoImagesInterval);
     return () => {
-      clearInterval(imagesInterval)
-    }
-  }, [])
+      clearInterval(imagesInterval);
+    };
+  }, []);
   return (
-    <div className='relative w-full overflow-hidden'>
-      <div className='flex transition-transform ease-in-out duration-500' style={{transform: `translateX(-${curentImg * 100}%)`}}>{images}</div>
+    <div className="relative w-full overflow-hidden">
+      <div
+        className="flex transition-transform ease-in-out duration-500"
+        style={{ transform: `translateX(-${curentImg * 100}%)` }}
+      >
+        {images}
+      </div>
       <div className="buttons absolute inset-0 flex items-center justify-between p-4">
-        <button onClick={prev} className='bg-white/30 text-gray-900 text-3xl cursor-pointer rounded-full p-1'>
-          <GrFormPrevious/>
+        <button
+          onClick={prev}
+          className="bg-white/30 text-gray-900 text-3xl cursor-pointer rounded-full p-1"
+        >
+          <GrFormPrevious />
         </button>
-        <button onClick={next} className='bg-white/30 text-gray-900 text-3xl cursor-pointer rounded-full p-1'>
-          <MdOutlineNavigateNext/>
+        <button
+          onClick={next}
+          className="bg-white/30 text-gray-900 text-3xl cursor-pointer rounded-full p-1"
+        >
+          <MdOutlineNavigateNext />
         </button>
       </div>
-      <div className='absolute bottom-2'>
-        <div className='flex items-center justify-center gap-2'>
-          {images.map((_, i) =>(
-            <div className={`transition-all w-2 h-2 rounded-full bg-white ${curentImg === i? 'p-1.5' : 'bg-opacity-50'}`}/>
+      <div className="absolute bottom-3 left-4">
+        <div className="flex items-center justify-center gap-2">
+          {images.map((_, i) => (
+            <div
+              className={`transition-all w-2 h-2 rounded-full bg-white ${
+                curentImg === i ? "p-1.5" : "bg-opacity-50"
+              }`}
+            />
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,22 +3,18 @@ import Navbar from "../../components/Navbar";
 import Carousel from "../../components/Carousel";
 
 // all images import
-import img1 from "../../assest/img/1 (1).jpg";
-import img2 from "../../assest/img/1 (2).jpg";
-import img3 from "../../assest/img/1 (3).jpg";
-import img4 from "../../assest/img/1 (4).jpg";
-import img5 from "../../assest/img/1 (5).jpg";
-import img6 from "../../assest/img/1 (6).jpg";
-import img7 from "../../assest/img/1 (7).jpg";
-import img8 from "../../assest/img/1 (8).jpg";
-
-let images = [img1, img2, img3, img4, img5, img6, img7, img8];
+import img1 from "../../assest/img/1.jpg";
+import img2 from "../../assest/img/2.jpg";
+import img3 from "../../assest/img/3.jpg";
+import img4 from "../../assest/img/4.jpg";
+import img5 from "../../assest/img/5.jpg";
+let images = [img1, img2, img3, img4, img5];
 
 function Home() {
   return (
     <div>
       <Navbar />
-      <div className="slider w-full ">
+      <div className="slider w-full">
         <Carousel autoImages = {true} autoImagesInterval={5000}>
           {images.map((image) => (
             <img src={image} alt="" />
