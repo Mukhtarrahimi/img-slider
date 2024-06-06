0e7cd1964b7867cde3b888b3e8c920ecc8d4ffe8
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-06 21:38:19 +0430

    update code

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar'
 function Blog() {
   return (
-    <div>
-      
+    <div className="about">
+      <Navbar/>
+      <div className='px-8 md:px-12 lg:px-16 flex flex-col items-center justify-center mt-10'>
+      <h1 className='text-2xl md:text-4xl text-green-700'>blog page</h1>
+      <p className='pt-6 text-gray-800 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore odio ut labore necessitatibus doloribus quod magni beatae corporis, in porro deserunt sequi minus. Voluptatum doloribus cupiditate fugiat officiis totam sit.</p>
+    </div>
     </div>
   )
 }
